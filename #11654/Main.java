38602492614835fc4ed024ff3469a6cc5821d612
Choi-Juntae <31373739+chlwnsxo00@users.noreply.github.com>
2022-05-21 05:47:13 +0900

    Add files via upload

---
+++ #11654/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) 
+	{
+		System.out.println("HI");
+		Scanner sc = new Scanner(System.in);
+		String num = sc.next();
+		System.out.println((int)num.charAt(0));
+		
+	}	
+}
